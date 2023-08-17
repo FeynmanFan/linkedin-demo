7712cbbe925527ae0f27b483ef298ec33d333294
Chris B. Behrens <cbehrens@gmail.com>
2023-08-17 17:37:28 -0500

    Initial add

---
+++ LIL.Weather/TemperatureConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace LIL.Weather
+{
+    public static class TemperatureConverter
+    {
+        public static double CelsiusToFahrenheit(double temperature)
+        {
+            return temperature * 8 / 5 + 32;
+        }
+    }
+}
